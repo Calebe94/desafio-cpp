4bac09987201d4127ac643c78ed2de1272beeeec
Calebe94 <calebe94@disroot.org>
2023-10-09 09:11:38 -0300

    Added multi files support
    - Added timeout to close inactive client

--- src/server.cpp
+++ src/server.cpp
@@ -4,10 +4,12 @@
 #include <boost/asio.hpp>
 #include <boost/property_tree/ptree.hpp>
 #include <boost/property_tree/json_parser.hpp>
+#include <boost/filesystem.hpp>
 
 #define DEFAULT_PORT          12345
 #define DEFAULT_PREFIX        "data"
 #define DEFAULT_MAX_FILE_SIZE 1024
+#define CLIENT_TIMEOUT        10
 
 using boost::asio::ip::tcp;
 
@@ -25,22 +27,18 @@ void handle_client(tcp::socket& socket, const std::string& prefix, std::size_t m
 
         auto now = std::chrono::system_clock::now();
         auto timestamp = std::chrono::system_clock::to_time_t(now);
-        std::string filename = prefix + "_" + std::to_string(timestamp) + ".txt";
-        std::ofstream file(filename, std::ios::binary);
 
-        if (!file.is_open())
-        {
-            std::cerr << "Error opening the file " << filename << std::endl;
-            return;
-        }
-
-        char buffer[1024];
+        char data[1024];
         std::size_t bytes_received;
+        std::size_t current_file_size = 0;
+        int file_index = 0;
+
+        boost::asio::deadline_timer timer(socket.get_executor());
 
         while (true)
         {
             boost::system::error_code error;
-            bytes_received = socket.read_some(boost::asio::buffer(buffer), error);
+            bytes_received = socket.read_some(boost::asio::buffer(data, max_file_size), error);
 
             if (error == boost::asio::error::eof)
             {
@@ -51,15 +49,50 @@ void handle_client(tcp::socket& socket, const std::string& prefix, std::size_t m
             {
                 throw boost::system::system_error(error);
             }
-
-            if (((size_t)file.tellp() + (size_t)bytes_received) > max_file_size)
+            else
             {
-                std::cerr << "Maximum file size reached. Closing file." << std::endl;
-                file.close();
-                break;
-            }
+                timer.expires_from_now(boost::posix_time::seconds(CLIENT_TIMEOUT));
+                timer.async_wait([&socket](const boost::system::error_code& ec){
+                    if (!ec)
+                    {
+                        std::cout << "Client timed out. Closing connection." << std::endl;
+                        socket.close();
+                    }
+                });
 
-            file.write(buffer, bytes_received);
+                std::string filename = prefix + "_" + std::to_string(timestamp);
+                if (file_index > 0)
+                {
+                    filename += "-" + std::to_string(file_index);
+                }
+                filename += ".txt";
+
+                if (current_file_size + bytes_received > max_file_size)
+                {
+                    std::cout << "Max size reached. Creating another file." << std::endl;
+                    current_file_size = 0;
+                    file_index++;
+                    filename = prefix + "_" + std::to_string(timestamp);
+                    if (file_index > 0)
+                    {
+                        filename += "-" + std::to_string(file_index);
+                    }
+                    filename += ".txt";
+                }
+
+                std::ofstream file(filename, std::ios_base::app);
+
+                if (!file.is_open())
+                {
+                    std::cerr << "Error opening the file " << filename << std::endl;
+                    return;
+                }
+
+                file.write(data, bytes_received);
+                file.close();
+
+                current_file_size += bytes_received;
+            }
         }
     }
     catch (std::exception& e)
@@ -87,7 +120,7 @@ int main(int argc, char* argv[])
         std::cout << "Configurações:" << std::endl;
         std::cout << "Porta: " << port << std::endl;
         std::cout << "Prefix: " << prefix << std::endl;
-        std::cout << "tamanho máximo: " << max_file_size << std::endl;
+        std::cout << "Tamanho máximo: " << max_file_size << " bytes" << std::endl;
 
         boost::asio::io_service io;
         tcp::acceptor acceptor(io, tcp::endpoint(tcp::v4(), port));
@@ -98,7 +131,7 @@ int main(int argc, char* argv[])
         {
             tcp::socket socket(io);
             acceptor.accept(socket);
-            std::thread(handle_client, std::ref(socket), prefix, max_file_size).detach();
+            handle_client(socket, prefix, max_file_size);
         }
     }
     catch (std::exception& e)
