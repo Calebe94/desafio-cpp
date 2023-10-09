ad058e6d9a9d64e8722960b00ac4b8532c34bd5b
Calebe94 <calebe94@disroot.org>
2023-10-09 10:50:49 -0300

    Extracted functions

--- src/server.cpp
+++ src/server.cpp
@@ -5,6 +5,7 @@
 #include <boost/property_tree/ptree.hpp>
 #include <boost/property_tree/json_parser.hpp>
 #include <boost/filesystem.hpp>
+#include <chrono>
 
 #define DEFAULT_PORT          12345
 #define DEFAULT_PREFIX        "data"
@@ -19,81 +20,70 @@ void usage(const char *name)
     exit(1);
 }
 
+std::string createFileWithIncrement(const std::string& prefix, int& file_index, const std::time_t& timestamp)
+{
+    std::string filename = prefix + "_" + std::to_string(timestamp);
+    if (file_index > 0)
+    {
+        filename += "-" + std::to_string(file_index);
+    }
+    filename += ".txt";
+    file_index++;
+    return filename;
+}
+
+void handleData(tcp::socket& socket, const std::string& prefix, std::size_t max_file_size)
+{
+    std::size_t current_file_size = 0;
+    int file_index = 0;
+    std::string filename;
+    auto now = std::chrono::system_clock::now();
+    auto timestamp = std::chrono::system_clock::to_time_t(now);
+    filename = createFileWithIncrement(prefix, file_index, timestamp);
+    while (true)
+    {
+        char data[1024];
+        boost::system::error_code error;
+        std::size_t bytes_received = socket.read_some(boost::asio::buffer(data, max_file_size), error);
+
+        if (error == boost::asio::error::eof)
+        {
+            std::cout << "Client disconnected." << std::endl;
+            break;
+        }
+        else if (error)
+        {
+            throw boost::system::system_error(error);
+        }
+
+        if (current_file_size + bytes_received > max_file_size)
+        {
+            std::cout << "Max size reached. Creating new file." << std::endl;
+            current_file_size = 0;
+            filename = createFileWithIncrement(prefix, file_index, timestamp);
+        }
+
+        std::ofstream file(filename, std::ios_base::app);
+        if (!file.is_open())
+        {
+            std::cerr << "Error opening the file " << filename << std::endl;
+            return;
+        }
+        file.write(data, bytes_received);
+        current_file_size += bytes_received;
+        file.close();
+    }
+}
+
 void handle_client(tcp::socket& socket, const std::string& prefix, std::size_t max_file_size)
 {
     try
     {
         std::cout << "Client connected!" << std::endl;
 
-        auto now = std::chrono::system_clock::now();
-        auto timestamp = std::chrono::system_clock::to_time_t(now);
-
-        char data[1024];
-        std::size_t bytes_received;
-        std::size_t current_file_size = 0;
-        int file_index = 0;
-
         boost::asio::deadline_timer timer(socket.get_executor());
 
-        while (true)
-        {
-            boost::system::error_code error;
-            bytes_received = socket.read_some(boost::asio::buffer(data, max_file_size), error);
-
-            if (error == boost::asio::error::eof)
-            {
-                std::cout << "Client disconnected." << std::endl;
-                break;
-            }
-            else if (error)
-            {
-                throw boost::system::system_error(error);
-            }
-            else
-            {
-                timer.expires_from_now(boost::posix_time::seconds(CLIENT_TIMEOUT));
-                timer.async_wait([&socket](const boost::system::error_code& ec){
-                    if (!ec)
-                    {
-                        std::cout << "Client timed out. Closing connection." << std::endl;
-                        socket.close();
-                    }
-                });
-
-                std::string filename = prefix + "_" + std::to_string(timestamp);
-                if (file_index > 0)
-                {
-                    filename += "-" + std::to_string(file_index);
-                }
-                filename += ".txt";
-
-                if (current_file_size + bytes_received > max_file_size)
-                {
-                    std::cout << "Max size reached. Creating another file." << std::endl;
-                    current_file_size = 0;
-                    file_index++;
-                    filename = prefix + "_" + std::to_string(timestamp);
-                    if (file_index > 0)
-                    {
-                        filename += "-" + std::to_string(file_index);
-                    }
-                    filename += ".txt";
-                }
-
-                std::ofstream file(filename, std::ios_base::app);
-
-                if (!file.is_open())
-                {
-                    std::cerr << "Error opening the file " << filename << std::endl;
-                    return;
-                }
-
-                file.write(data, bytes_received);
-                file.close();
-
-                current_file_size += bytes_received;
-            }
-        }
+        handleData(socket, prefix, max_file_size);
     }
     catch (std::exception& e)
     {
