ff3f1cf201e221a64766522b3cfdfe89e5c3e7d1
Calebe94 <calebe94@disroot.org>
2023-10-09 12:43:20 -0300

    Added a debug method to the client

--- src/client.cpp
+++ src/client.cpp
@@ -2,22 +2,25 @@
 #include <string>
 #include <fstream>
 #include <boost/asio.hpp>
+#include <cstdlib>
+#include <thread>
 
 using boost::asio::ip::tcp;
 
-int main(int argc, char* argv[])
-{
-    if (argc != 3)
-    {
-        std::cerr << "Usage: " << argv[0] << " <host> <port>" << std::endl;
-        return 1;
+bool parseCommandLine(int argc, char* argv[], std::string& host, std::string& port, bool& isDebug) {
+    if (argc < 3) {
+        std::cerr << "Usage: " << argv[0] << " <host> <port> [debug]" << std::endl;
+        return false;
     }
 
-    const std::string host = argv[1];
-    const std::string port = argv[2];
+    host = argv[1];
+    port = argv[2];
+    isDebug = (argc == 4 && std::string(argv[3]) == "debug");
+    return true;
+}
 
-    try
-    {
+void runClient(const std::string& host, const std::string& port, bool isDebug) {
+    try {
         boost::asio::io_service io_service;
         tcp::socket socket(io_service);
         tcp::resolver resolver(io_service);
@@ -27,26 +30,45 @@ int main(int argc, char* argv[])
 
         boost::asio::connect(socket, endpoint_iterator);
 
-        while (true)
-        {
-            std::string message;
-            std::cout << "Enter a message to send to the server (or 'quit' to exit): ";
-            std::getline(std::cin, message);
+        if (isDebug) {
+            std::cout << "Debug Mode!" << std::endl;
+            while (true) {
+                std::string message = "Debug Message: " + std::to_string(std::rand());
+                std::cout << "Sending a debug message: " << message << std::endl;
+                boost::asio::write(socket, boost::asio::buffer(message + "\n"));
 
-            if (message == "quit")
-            {
-                break;
+                ::sleep(1);
             }
+        } else {
+            while (true) {
+                std::string message;
+                std::cout << "Enter a message to send to the server (or 'quit' to exit): ";
+                std::getline(std::cin, message);
 
-            boost::asio::write(socket, boost::asio::buffer(message + "\n"));
+                if (message == "quit") {
+                    break;
+                }
+
+                boost::asio::write(socket, boost::asio::buffer(message + "\n"));
+            }
         }
 
         socket.close();
-    }
-    catch (std::exception& e)
-    {
+    } catch (std::exception& e) {
         std::cerr << "Error: " << e.what() << std::endl;
     }
+}
+
+int main(int argc, char* argv[]) {
+    std::string host;
+    std::string port;
+    bool isDebug;
+
+    if (!parseCommandLine(argc, argv, host, port, isDebug)) {
+        return 1;
+    }
+
+    runClient(host, port, isDebug);
 
     return 0;
 }
